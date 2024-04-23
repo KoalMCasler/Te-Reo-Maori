fd91bc360b61e9ecae35f1748e01fa532c79fc67
Adrian Dorey <144232716+adorey91@users.noreply.github.com>
2024-04-23 09:46:52 -0300

    Added sprites

---
+++ Assets/Sprites/Castle Interiors 1.1/sample map (requires Tiled)/bonus shadows.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="2021.03.23" name="bonus shadows" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+ <image source="bonus shadows.png" width="128" height="128"/>
+</tileset>
---
+++ Assets/Sprites/Castle Interiors 1.1/sample map (requires Tiled)/castle interiors.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="castle interiors" tilewidth="16" tileheight="16" tilecount="512" columns="32">
+ <image source="castle interiors.png" width="512" height="256"/>
+</tileset>
