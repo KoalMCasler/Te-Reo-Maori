9bf42a7c7806b9f297bd973f69243817d1694203
Koal M. Casler <71401318+KoalMCasler@users.noreply.github.com>
2024-04-24 10:48:35 -0300

    Info object interactions added.

--- Assets/Scripts/Interactions/InteractableObject.cs
+++ Assets/Scripts/Interactions/InteractableObject.cs
@@ -1,24 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class InteractableObject : MonoBehaviour
 {
+    //All objects
     public enum InteractType
     {
+        Nothing,
         Info,
         Dialogue,
         Book,
         Door
     }
-
+    [Header("Interaction Type")]
+    [SerializeField]
     public InteractType interactType;
-
+    [Header("Door Variables")]
+    [SerializeField]
+    //Doors
+    public bool IsLocked;
+    public LevelManager levelManager;
+    [Header("Info Object variables")]
+    [SerializeField]
+    //Info
+    private TextMeshProUGUI infoText;
+    public float InfoTextDelay;
+    public string message;
+    public float textSpeed = 0.01f;
+    [Header("NPC Variables")]
+    [SerializeField]
+    //NPCS
     public Dialogue dialogue;
+    [Header("Scriptable Object")]
+    public ScriptableIO scriptableIO;
+    public ScriptableNPC scriptableNPC;
+    void Start()
+    {
+        levelManager = FindObjectOfType<LevelManager>();
+        IsLocked = true;
+        infoText = GameObject.Find("InfoText").GetComponent<TextMeshProUGUI>();
+        infoText.text = null;
+        if(interactType == InteractType.Nothing)
+        {
+            Debug.Log(this.name + " Has a type of nothing, Was this by mistake?");
+        }
+    }
 
     public void Dialogue()
     {
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
     }
      
+    public void Info()
+    {
+        Debug.Log("Reading info from " + this.name);
+        //Debug.Log(message);
+        StartCoroutine(ShowInfo(message, InfoTextDelay));
+    }
+
+    IEnumerator ShowInfo(string message, float delay)
+    {
+        StartCoroutine(ScrollingText(message));
+        yield return new WaitForSeconds(delay);
+        infoText.text = "";
+    }
+
+    private IEnumerator ScrollingText(string currentLine)
+    {
+        for(int i = 0; i < currentLine.Length + 1; i++)
+        {
+            infoText.text = currentLine.Substring(0,i);
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
 }
--- Assets/Scripts/Player/PlayerInteraction.cs
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInteraction : MonoBehaviour
 {
+
+    public GameObject currentInterObj = null;
+    public InteractableObject currentInterObjScript = null;
+    public GameObject indicator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        indicator.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if(other.CompareTag("Interactable") == true)
+        {
+            currentInterObj = other.gameObject;
+            currentInterObjScript = currentInterObj.GetComponent<InteractableObject>();
+            indicator.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag("Interactable") == true)  
+        {
+            currentInterObj = null;
+            indicator.SetActive(false);
+        }
+    }
+
+    void OnInteract()
+    {
+        if(currentInterObj == true)
+        {
+            if(currentInterObjScript.interactType == InteractableObject.InteractType.Info)
+            {
+                currentInterObjScript.Info();
+            }
+        }
+        else
+        {
+            Debug.Log("Nothing to interact with.");
+        }
     }
 }
